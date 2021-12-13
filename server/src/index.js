c34b1430bbfd898a88aba4518819c33a862ae909
Asdaois <josexx1234@gmail.com>
2021-12-13 18:40:37 -0400

    feat(server): basic server finalized

--- server/src/index.js
+++ server/src/index.js
@@ -1,17 +1,28 @@
 import express from "express";
 import cors from "cors";
+import morgan from "morgan";
 
 const app = express();
 
 //middlewares
+app.use(
+  cors({
+    origin: [
+      "https://odin-book-client.herokuapp.com/",
+      "http://localhost:3000",
+    ],
+  })
+);
+
+app.use(morgan("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(cors());
 
 app.get("/", (req, res) => {
   res.json("Nothing here");
 });
 
-app.listen(process.env.PORT || 3001, () => {
-  console.log("Server is running on port 3001!");
+const port = process.env.PORT || 3001;
+app.listen(port, () => {
+  console.log(`\x1b Server is running on port ${port}!\x1b`);
 });
