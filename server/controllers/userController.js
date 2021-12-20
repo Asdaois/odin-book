1a3b38dc47b07ae264df08c6c789f2486aaf5218
Jose Guevara <josexx1234@gmail.com>
2021-12-20 08:20:06 -0400

    docs(userController): add documentation

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -1,8 +1,12 @@
 import User from "../models/User.js";
 import Profile from "../models/Profile.js";
 
-// Las busquedas en la base de datos estaran comentadas hasta que se implemente la base
-
+/**
+ * Las busquedas en la base de datos estaran comentadas hasta que se implemente la base
+ * @param {Request} req 
+ * @param {Response} res 
+ * @param {NextFunction} next 
+ */
 const getUser = (req, res, next) => {
   // await User.find({ _id: req.params.id }).exec((err, result) => {
   //     if (err) {
@@ -10,10 +14,14 @@ const getUser = (req, res, next) => {
   //     }
   //     res.json({ result });
   //   });
-
   res.status(200).json({ message: "User Index" });
 };
 
+/**
+ * @param {Request} req 
+ * @param {Response} res 
+ * @param {NextFunction} next 
+ */
 const getProfile = (req, res, next) => {
   // await Profile.find({ _id: req.params.id }).exec((err, result) => {
   //     if (err) {
@@ -21,10 +29,14 @@ const getProfile = (req, res, next) => {
   //     }
   //     res.json({ result });
   //   });
-
   res.status(200).json({ message: "User Profile" });
 };
 
+/**
+ * @param {Request} req 
+ * @param {Response} res 
+ * @param {NextFunction} next 
+ */
 const getFriendList = (req, res, next) => {
   // await Profile.find({ _id: req.params.id })
   //   .select("friends")
@@ -35,6 +47,11 @@ const getFriendList = (req, res, next) => {
   res.status(200).json({ message: "User's friends" });
 };
 
+/**
+ * @param {Request} req 
+ * @param {Response} res 
+ * @param {NextFunction} next 
+ */
 const createNewUser = async (req, res, next) => {
   // const {
   //   firstName,
@@ -61,6 +78,11 @@ const createNewUser = async (req, res, next) => {
   res.status(200).json({ message: "User created" });
 };
 
+/**
+ * @param {Request} req 
+ * @param {Response} res 
+ * @param {NextFunction} next 
+ */
 const updateUser = async (req, res, next) => {
   // const {
   //   firstName,
@@ -88,6 +110,11 @@ const updateUser = async (req, res, next) => {
   res.status(200).json({ message: "User updated" });
 };
 
+/**
+ * @param {Request} req 
+ * @param {Response} res 
+ * @param {NextFunction} next 
+ */
 const deleteUser = async (req, res, next) => {
   // await User.findOneAndDelete({ _id: req.params.id }, (err) => {
   //   if (err) {
--- server/src/index.js
+++ server/src/index.js
@@ -28,6 +28,6 @@ app.use("/users", userRouter);
 
 const port = process.env.PORT || 3001;
 app.listen(port, () => {
-  console.log(`\x1b Server is running on port ${port}!\x1b`);
+  console.log(`Server is running on port ${port}!`);
   connectToDatabase();
 });
