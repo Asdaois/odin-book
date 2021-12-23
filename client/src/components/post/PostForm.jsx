4c34f697ff2813bf0c77b190d174dac6e5f95380
Jose Guevara <josexx1234@gmail.com>
2021-12-23 17:52:11 -0400

    feat(post form): Add request to database

--- client/src/components/post/PostForm.jsx
+++ client/src/components/post/PostForm.jsx
@@ -1,8 +1,35 @@
 import React from "react";
+import { useSelector } from "react-redux";
+import { postApi } from "../../api/postApi";
+import { userApi } from "../../api/usersApi";
 import AutoSizeTextArea from "../custom_inputs/AutoSizeTextArea";
 import { NameWithPicture } from "../user";
 
 const PostForm = ({ onExit }) => {
+  const user = useSelector((state) => state.user);
+
+  /** Handle Post submit
+   * @param {React.FormEvent<HTMLFormElement>} e 
+   */
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    let userID = user.current._id
+
+    if (!userID) {
+      const dbUser = await userApi.getByUid(user.current.uid);
+      userID = dbUser._id;
+    }
+
+    const post = {
+      userID,
+      content: {
+        text: e.target.children['contentText'].value
+      }
+    }
+    
+    postApi.create(post);
+  };
+
   return (
     <div className="w-full">
       <div className="w-full text-center flex justify-between items-center">
@@ -15,14 +42,15 @@ const PostForm = ({ onExit }) => {
         </div>
       </div>
       <hr className="w-full mt-2" />
-      <form action="">
-      <AutoSizeTextArea />
-      {/* TODO: When there are not data on teh textarea block the bottom */}
-      <button
-        className="w-full rounded-3xl text-center bg-blue-400 hover:bg-blue-300 "
-      >
-        Post
-      </button>
+      <form onSubmit={handleSubmit}>
+        <AutoSizeTextArea name={"contentText"} />
+        {/* TODO: When there are not data on teh textarea block the bottom */}
+        <button
+          className="w-full rounded-3xl text-center bg-blue-400 hover:bg-blue-300 "
+          type="submit"
+        >
+          Post
+        </button>
       </form>
     </div>
   );
