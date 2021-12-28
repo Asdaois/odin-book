57e372cda9e4c5f07984a3ee4aca654565fd86b1
Jose Guevara <josexx1234@gmail.com>
2021-12-27 21:57:24 -0400

    feat(Posts.jsx): Show last 5 posts

--- client/src/components/post/Post.jsx
+++ client/src/components/post/Post.jsx
@@ -1,28 +1,26 @@
 import React from "react";
 import ProfilePicture from "../user/profile_picture/ProfilePicture";
 import { Comment, Like } from "../icons";
-import TimeAgo from 'timeago-react'
+import TimeAgo from "timeago-react";
 
 const Post = ({ post }) => {
+  console.log(post);
+  const createAT = post.createdAt
+  const date = new Date(createAT)
+
   return (
     <div className="w-full bg-slate-200 mt-4 rounded-xl">
       <div className="flex items-center p-4 pb-0">
-        <ProfilePicture pictureLink={"http://placeimg.com/640/480 "} />
+        <ProfilePicture pictureLink={post.userID?.avatar} />
         <div className="flex flex-col ml-2">
-          <span>{/* TODO: Here the displayName */ "Display Name"}</span>
+          <span>{post.userID.displayName}</span>
           <span className="text-xs">
-            <TimeAgo
-              datetime={/* TODO: Here the creation time*/ 1640633896401}
-            />
+            <TimeAgo datetime={date} datatype='' />
           </span>
         </div>
       </div>
       <div className="">
-        <p className="p-4">
-          {
-            /* TODO: Here the content */ "Et nesciunt cupiditate corporis et numquam. Nostrum voluptas neque voluptates. Sit autem et quas. Sit qui necessitatibus officiis magni. Occaecati alias voluptatem voluptas et sequi repudiandae voluptates. Exercitationem enim sint natus rerum vitae molestias et."
-          }
-        </p>
+        <p className="p-4">{post.content?.text}</p>
       </div>
       <div class="relative">
         <div class="w-[90%] m-auto border-t border-gray-300"></div>
--- client/src/components/posts/Posts.jsx
+++ client/src/components/posts/Posts.jsx
@@ -1,12 +1,26 @@
-import React from 'react'
-import { Post } from '../post'
+import React from "react";
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { getLastPosts } from "../../features/posts/posts.thunks";
+import { Post } from "../post";
 
 const Posts = () => {
+  const posts = useSelector((state) => state.posts);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(getLastPosts());
+  }, []);
+
+  useEffect(() => {
+    console.log(posts.lasts)
+  }, [posts])
+
   return (
     <div>
-      <Post />
+      {posts.lasts?.map((post)=> <Post post={post}/>)}
     </div>
-  )
-}
+  );
+};
 
-export default Posts
+export default Posts;
