11f557335c4fba2b8a435102fa546ca51d13eba5
Jose Guevara <josexx1234@gmail.com>
2022-01-06 14:08:08 -0400

    fix(profilePicture): size of the image

--- client/src/components/comment/CommentInput.jsx
+++ client/src/components/comment/CommentInput.jsx
@@ -4,27 +4,31 @@ import { useSelector } from 'react-redux';
 
 import ProfilePicture from '../user/profile_picture';
 
-const CommentInput = ({postID}) => {
+const CommentInput = ({ postID }) => {
   const user = useSelector((state) => state.user);
   /**
    * @param {React.FormEvent<HTMLFormElement>} e
    */
   const handleSubmit = (e) => {
     e.preventDefault();
-    console.log(e.currentTarget.elements);
-
-  }
+    // console.log(e.currentTarget.elements);
+  };
 
   return (
-    <div className="flex items-center gap-2">
+    <div className="flex justify-between items-center w-full gap-2">
       <ProfilePicture />
-      <form action={''} method="post" className="w-full flex items-center" onSubmit={handleSubmit}>
+      <form
+        action={"" /*TODO: Here the path to the server*/}
+        method="post"
+        className="w-full flex items-center"
+        onSubmit={handleSubmit}
+      >
         <input type="hidden" name="userID" value={user.current?._id} />
-        <div className="rounded-3xl bg-slate-400 px-2 w-full h-10 flex items-center gap-2 box-border">
+        <div className="rounded-3xl bg-slate-400 px-4 w-full h-10 flex items-center">
           <input
             type="text"
             name="content-text"
-            className="bg-transparent outline-none pl-2 w-full placeholder-slate-700"
+            className="bg-transparent outline-none w-full placeholder-slate-700"
             placeholder="Write a comment..."
           />
         </div>
--- client/src/components/user/profile_picture/ProfilePicture.jsx
+++ client/src/components/user/profile_picture/ProfilePicture.jsx
@@ -2,15 +2,13 @@ import React from 'react';
 
 import emptyProfile from './emptyProfile.png';
 
-const ProfilePicture = ({pictureLink, small}) => {
+const ProfilePicture = ({ pictureLink, small }) => {
   return (
-    <div>
-      <img
-        src={pictureLink || emptyProfile}
-        alt="profile"
-        className={`rounded-full aspect-auto ${small ? 'h-5 w-5': 'h-10 w-10'}`}
-      />
-    </div>
+    <img
+      src={pictureLink || emptyProfile}
+      alt="profile"
+      className={`rounded-full aspect-auto ${small ? "h-5 w-5" : "h-10 w-10"}`}
+    />
   );
 };
 
