65aae1601dcd65bf55170a106b3952f5fb8b94f2
Jose Guevara <josexx1234@gmail.com>
2022-01-06 21:37:14 -0400

    docs(comment-content): explain the component
    #11

--- client/src/components/comment/CommentContent.jsx
+++ client/src/components/comment/CommentContent.jsx
@@ -5,9 +5,17 @@ import moment from 'moment';
 import ProfilePicture from '../user/profile_picture';
 import { CommentInput } from './';
 
-const CommentContent = ({ comment, isReply }) => {
+/**
+ *
+ * @param props
+ * @param {string} props.owner Is a id, This is for when it is a child of a comment to decide whether to add a new comment to the parent or to itself.
+ * @param {boolean} props.isReply Handle the size of the profilePicture image
+ * @returns {React.Component}
+ */
+const CommentContent = ({ comment, isReply, owner }) => {
   const [isReplying, setIsReplying] = useState(false);
-  const { content, userID: user, comments, postType } = comment;
+  const { content, userID: user, comments, postType, _id } = comment;
+  const postID = owner || comment._id;
 
   return (
     <div className="">
@@ -23,25 +31,27 @@ const CommentContent = ({ comment, isReply }) => {
           <div className="text-xs px-2">
             <span className="hover:cursor-pointer">Like</span>
             &nbsp;·&nbsp;
-            <span className="hover:cursor-pointer" onClick={()=>setIsReplying(true)}>Reply</span>
+            <span className="hover:cursor-pointer" onClick={() => setIsReplying(true)}>
+              Reply
+            </span>
             &nbsp;·&nbsp;
             <span>{moment(new Date(comment.createdAt)).fromNow()}</span>
           </div>
         </div>
       </div>
 
+      {/* Recursion in react for print nested comments */}
       {postType === 'Comment' && (
         <div className="ml-12 mt-2">
           {comments?.map((comment) => (
-            <CommentContent comment={comment} isReply={true} key={comment._id} />
+            <CommentContent comment={comment} isReply={true} key={comment._id} owner={_id} />
           ))}
-          {postType === 'Comment' && (
-            <div className={`${isReplying ? 'block' : 'hidden'}`}>
-              <CommentInput postID={comment._id} postType={'SubComment'} />
-            </div>
-          )}
         </div>
       )}
+
+      <div className={`${isReplying ? 'block' : 'hidden'}`}>
+        <CommentInput postID={postID} postType={'SubComment'} />
+      </div>
     </div>
   );
 };
