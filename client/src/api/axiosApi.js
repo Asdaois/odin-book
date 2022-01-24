214e565c11a8b5539c347558e72a8979565217e8
Jose Guevara <josexx1234@gmail.com>
2022-01-24 11:40:47 -0400

    fix: add forgotten files

--- client/src/api/axiosApi.js
+++ client/src/api/axiosApi.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 // TODO: This is to change later on
-const URL = process.env.REACT_APP_SERVER_URL ?? 'http://localhost:3001/'
+const URL = process.env.REACT_APP_SERVER_URL || 'http://localhost:3001/'
 const axiosApi = axios.create({ baseURL: URL })
 axiosApi.defaults.headers.post['Content-Type'] = 'application/json'
 
---
+++ server/src/app.js
@@ -0,0 +1,37 @@
+import express from 'express';
+import cors from 'cors';
+import userRouter from './routes/userRouter.js';
+import postRouter from './routes/postRouter.js';
+import requestLogger from './middlewares/requestLogger.js';
+import errorHandler from './middlewares/errorHandler.js';
+import unknowEndpoint from './middlewares/unknownEndpoint.js';
+import connectToDatabase from './database.js';
+
+const app = express();
+connectToDatabase();
+
+// middlewares
+app.use(
+  cors({
+    origin: [
+      'https://odin-book-client.herokuapp.com/',
+      'http://localhost:3000',
+    ],
+  }),
+);
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use(requestLogger);
+
+app.use(express.static('../client/build'));
+// end middlewares
+
+app.use('/users', userRouter);
+app.use('/posts', postRouter);
+
+app.use(unknowEndpoint);
+app.use(errorHandler);
+
+export default app;
